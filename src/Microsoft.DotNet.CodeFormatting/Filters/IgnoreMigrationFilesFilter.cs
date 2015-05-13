cf255f4c96bd3961fa09d8d2ba38a5c9895fc3d7
HellBrick <hellbrick@webkontrol.ru>
2015-05-13 18:19:18 +0300

    Added a filter to disable scanning migration files

---
+++ src/Microsoft.DotNet.CodeFormatting/Filters/IgnoreMigrationFilesFilter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+
+namespace Microsoft.DotNet.CodeFormatting.Filters
+{
+	[Export( typeof( IFormattingFilter ) )]
+	internal sealed class IgnoreMigrationFilesFilter : IFormattingFilter
+	{
+		private readonly Options _options;
+
+		[ImportingConstructor]
+		public IgnoreMigrationFilesFilter( Options options )
+		{
+			_options = options;
+		}
+
+		public bool ShouldBeProcessed( Document document )
+		{
+			string parentFolderName = Path.GetFileName( Path.GetDirectoryName( document.FilePath ) );
+			return parentFolderName != "Migrations";
+		}
+	}
+}
